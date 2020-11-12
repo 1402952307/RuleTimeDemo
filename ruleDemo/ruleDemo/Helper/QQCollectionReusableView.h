df0f1ae5491e9796387464c9ecfcff3113f0406b
huangyouli <1402952307@qq.com>
2020-11-12 19:18:49 +0800

    上传文件

---
+++ ruleDemo/ruleDemo/Helper/QQCollectionReusableView.h
@@ -0,0 +1,17 @@
+//
+//  QQCollectionReusableView.h
+//  ruleDemo
+//
+//  Created by mac02 on 2020/11/12.
+//  Copyright © 2020 HaiLai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QQCollectionReusableView : UICollectionReusableView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ruleDemo/ruleDemo/Helper/QQCollectionReusableView.m
@@ -0,0 +1,13 @@
+//
+//  QQCollectionReusableView.m
+//  ruleDemo
+//
+//  Created by mac02 on 2020/11/12.
+//  Copyright © 2020 HaiLai. All rights reserved.
+//
+
+#import "QQCollectionReusableView.h"
+
+@implementation QQCollectionReusableView
+
+@end
